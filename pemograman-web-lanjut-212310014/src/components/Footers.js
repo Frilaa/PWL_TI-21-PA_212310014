07c2bf7e0c5172a4bac7becca8d2d396cdd39c7b
Frilaa <113754630+Frilaa@users.noreply.github.com>
2024-06-17 17:48:18 +0700

    Tugas dan Quiz

--- pemograman-web-lanjut-212310014/src/components/Footers.js
+++ pemograman-web-lanjut-212310014/src/components/Footers.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from 'react'
 
-const Footer = () => {
+export default function Footers() {
     return (
-        <div className='text-center'>
-            <div className=''>
-                <div className=''>
-                    <span className=''>2024 &copy;</span>
-                    <span className=''>IBI Kesatuan Bogor</span>
-                </div>
-            </div>
-        </div>
-    );
-}
+        <div className="container">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <p className="col-md-4 mb-0 text-body-secondary">&copy; 2024 Company, Inc</p>
 
-export default Footer;
+                <ul className="nav col-md-4 justify-content-end">
+                    <li className="nav-item"><a className="nav-link px-2 text-body-secondary">Home</a></li>
+                    <li className="nav-item"><a className="nav-link px-2 text-body-secondary">Features</a></li>
+                    <li className="nav-item"><a className="nav-link px-2 text-body-secondary">Pricing</a></li>
+                    <li className="nav-item"><a className="nav-link px-2 text-body-secondary">FAQs</a></li>
+                    <li className="nav-item"><a className="nav-link px-2 text-body-secondary">About</a></li>
+                </ul>
+            </footer>
+        </div>
+    )
+}
